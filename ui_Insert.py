e922b6e397221d0fc875291e6d80cbca6915af11
mahankabir <mahan.kabir@gmail.com>
2021-06-02 15:58:50 +0430

    fifth commit

--- ui_Insert.py
+++ ui_Insert.py
@@ -1,8 +1,33 @@
 from tkinter import *
+import pymysql
 
+
+con = pymysql.connect(host="localhost", user="poulstar", password="poulstar", database="my_db_n")
 
 def add():
-    print("Hello")
+    sql = "INSERT INTO users1(name, family, age) VALUES(%s,%s,%s)"
+    data =(var1.get(), var2.get(), var3.get())
+    try:
+        with con.cursor() as cr:
+            cr.execute(sql, data)
+            con.commit()
+    except:
+        print("Access denied!")
+
+def update():
+    sql = "UPDATE users1 SET `family`= 'Rahimizadeh' WHERE `name`='Amir'"
+    try:
+        with con.cursor() as cr:
+            cr.execute(sql)
+            con.commit()
+    except:
+        print("Access denied!")    
+
+def delete():
+    pass
+
+def select():
+    pass
 
 root = Tk()
 
@@ -29,4 +54,14 @@ e3.grid(row=2, column=1)
 
 b1 = Button(root, text="Register", command=add)
 b1.grid(row=3, column=0, columnspan=2, sticky='we')
+
+b2 = Button(root, text="Update", command=update)
+b2.grid(row=4, column=0, columnspan=2, sticky='we')
+
+b3 = Button(root, text="Delete", command=delete)
+b3.grid(row=5, column=0, columnspan=2, sticky='we')
+
+b4 = Button(root, text="Select", command=select)
+b4.grid(row=6, column=0, columnspan=2, sticky='we')
+
 root.mainloop()
